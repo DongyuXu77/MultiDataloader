19da1a206a76d36bd67a1262c9f4e8bd6f21807f
DongyuXu77 <xudongyu@bupt.edu.com>
2022-09-04 22:39:38 +0800

    fix some bugs

--- test/test_naiveDataloader.py
+++ test/test_naiveDataloader.py
@@ -1,11 +1,17 @@
-from loader.naive import NaiveDataloader
 from dataset.dataset import TestDataset
+from loader.naive import NaiveDataloader
+from loader.multi import MultiDataloader
+
 def test():
     dataset = TestDataset(fake_len=100)
-    dataloader = NaiveDataloader(dataset, batch_size=16, drop_last=False)
-    print(len(dataloader))
-    for _ in dataloader:
-        ...
+    dataloader_naive = NaiveDataloader(dataset, batch_size=16, drop_last=False)
+    dataloader_multi = MultiDataloader(dataset, batch_size=16, drop_last=False)
+    print(len(dataloader_naive))
+    for data in dataloader_naive:
+        print(data)
+    print('#'*100)
+    for data in dataloader_multi:
+        print(data)
 
 if __name__=="__main__":
     test()
