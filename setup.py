303435a905951f69e9e2cb80f6c08eac73076417
XUDONGYU <xudongyu@bupt.edu.cn>
2022-09-05 11:21:16 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version = "0.0.1",
     author = "Dongyu Xu",
     author_email = "xudongyu@bupt.edu.cn",
-    description = "a toy package to test diff dataloader performance",
+    description = "a toy package to test diff dataloaders performance",
     packages = find_packages(
         exclude = ['*.md', 'test/*']
     )
