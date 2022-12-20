0d598522b9304d27743f6e6fcf985fe5f8137bf6
DongyuXu77 <xudongyu@bupt.edu.com>
2022-12-20 16:39:22 +0800

    test 100,000 data

--- test/test.py
+++ test/test.py
@@ -5,7 +5,7 @@ from dataset.dataset import TestDataset
 from loader.naive import NaiveDataloader
 from loader.multi import MultiDataloader
 
-BATCH_SIZE = 16
+BATCH_SIZE = 256
 def train_with_naive(dataset, dataset_len):
     dataloader_naive = NaiveDataloader(dataset, batch_size=BATCH_SIZE, drop_last=False)
     start_time = time.time()
@@ -23,7 +23,7 @@ def train_with_multi(dataset, dataset_len):
     print(f"multi_time (dataset_len:{dataset_len} batch_size:{BATCH_SIZE}): {multi_time: .4f}")
 
 def test():
-    dataset_len = [1000*time for time in range(10, 11)]
+    dataset_len = [10000*time for time in range(10, 11)]
     for index_1 in range(len(dataset_len)):
         process_1 = Process(target=train_with_naive, args=(TestDataset(fake_len=dataset_len[index_1]), dataset_len[index_1], ), )
         process_2 = Process(target=train_with_multi, args=(TestDataset(fake_len=dataset_len[index_1]), dataset_len[index_1], ), )
